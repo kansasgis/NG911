f32a9ca03ae73e9c595cb222789f67c6cdccb564
KANSASGIS <kristen@kgs.ku.edu>
2015-09-08 11:50:06 -0500

    Removed alt county locator for TN geocoding
    Edit confirmed by Sherry Massey

--- Scripts/Enhancement_CheckTN.py
+++ Scripts/Enhancement_CheckTN.py
@@ -21,7 +21,6 @@ def createLocators(gdb_object):
     AL1 = join(gdb_object.gdbPath, "AddressLocator")
     AL2 = join(gdb_object.gdbPath, "RoadLocator")
     AL3 = join(gdb_object.gdbPath, "CompositeLoc")
-    AL4 = join(gdb_object.gdbPath, "AltCountyLoc")
 
     if not Exists(AL1):
         #Create address locator from NG911 Address points AL1
@@ -100,57 +99,6 @@ def createLocators(gdb_object):
             if Exists(AL2):
                 userMessage("Created road centerline locator")
 
-        if not Exists(AL4):
-            #Create address locator from copy of Road centerline using the county field as the city field AL4
-            AL4_fieldMap = """'Primary Table:Feature ID' <None> VISIBLE NONE;'*Primary Table:From Left' RoadCenterline:L_F_ADD VISIBLE NONE;
-                '*Primary Table:To Left' RoadCenterline:L_T_ADD VISIBLE NONE;'*Primary Table:From Right' RoadCenterline:R_F_ADD VISIBLE NONE;
-                '*Primary Table:To Right' RoadCenterline:R_T_ADD VISIBLE NONE;'Primary Table:Prefix Direction' RoadCenterline:PRD VISIBLE NONE;
-                'Primary Table:Prefix Type' RoadCenterline:STP VISIBLE NONE;'*Primary Table:Street Name' RoadCenterline:RD VISIBLE NONE;
-                'Primary Table:Suffix Type' RoadCenterline:STS VISIBLE NONE;'Primary Table:Suffix Direction' RoadCenterline:POD VISIBLE NONE;
-                'Primary Table:Left City or Place' RoadCenterline:COUNTY_L VISIBLE NONE;
-                'Primary Table:Right City or Place' RoadCenterline:COUNTY_R VISIBLE NONE;
-                'Primary Table:Left ZIP Code' RoadCenterline:ZIP_L VISIBLE NONE;'Primary Table:Right ZIP Code' RoadCenterline:ZIP_R VISIBLE NONE;
-                'Primary Table:Left State' RoadCenterline:STATE_L VISIBLE NONE;'Primary Table:Right State' RoadCenterline:STATE_R VISIBLE NONE;
-                'Primary Table:Left Street ID' <None> VISIBLE NONE;'Primary Table:Right Street ID' <None> VISIBLE NONE;
-                'Primary Table:Min X value for extent' <None> VISIBLE NONE;'Primary Table:Max X value for extent' <None> VISIBLE NONE;
-                'Primary Table:Min Y value for extent' <None> VISIBLE NONE;'Primary Table:Max Y value for extent' <None> VISIBLE NONE;
-                'Primary Table:Left Additional Field' <None> VISIBLE NONE;'Primary Table:Right Additional Field' <None> VISIBLE NONE;
-                'Primary Table:Altname JoinID' RoadCenterline:SEGID VISIBLE NONE;'*Alternate Name Table:JoinID' RoadAlias:SEGID VISIBLE NONE;
-                'Alternate Name Table:Prefix Direction' RoadAlias:A_PRD VISIBLE NONE;'Alternate Name Table:Prefix Type' RoadAlias:A_STP VISIBLE NONE;
-                'Alternate Name Table:Street Name' RoadAlias:A_RD VISIBLE NONE;'Alternate Name Table:Suffix Type' RoadAlias:A_STS VISIBLE NONE;
-                'Alternate Name Table:Suffix Direction' RoadAlias:A_POD VISIBLE NONE"""
-
-            userMessage("Creating county alternate locator from road centerlines...")
-
-            try:
-                CreateAddressLocator_geocoding("US Address - Dual Ranges", streetPath + " 'Primary Table';" + roadAliasPath + " 'Alternate Name Table'", AL4_fieldMap, AL4, "")
-            except:
-                try:
-                    AL4_fieldMap = """'Primary Table:Feature ID' <None> VISIBLE NONE;'*Primary Table:From Left' RoadCenterline:L_F_ADD VISIBLE NONE;
-                    '*Primary Table:To Left' RoadCenterline:L_T_ADD VISIBLE NONE;'*Primary Table:From Right' RoadCenterline:R_F_ADD VISIBLE NONE;
-                    '*Primary Table:To Right' RoadCenterline:R_T_ADD VISIBLE NONE;'Primary Table:Prefix Direction' RoadCenterline:PRD VISIBLE NONE;
-                    'Primary Table:Prefix Type' RoadCenterline:STP VISIBLE NONE;'*Primary Table:Street Name' RoadCenterline:RD VISIBLE NONE;
-                    'Primary Table:Suffix Type' RoadCenterline:STS VISIBLE NONE;'Primary Table:Suffix Direction' RoadCenterline:POD VISIBLE NONE;
-                    'Primary Table:Left City or Place' RoadCenterline:COUNTY_L VISIBLE NONE;
-                    'Primary Table:Right City or Place' RoadCenterline:COUNTY_R VISIBLE NONE;
-                    'Primary Table:Left ZIP Code' RoadCenterline:ZIP_L VISIBLE NONE;'Primary Table:Right ZIP Code' RoadCenterline:ZIP_R VISIBLE NONE;
-                    'Primary Table:Left State' RoadCenterline:STATE_L VISIBLE NONE;'Primary Table:Right State' RoadCenterline:STATE_R VISIBLE NONE;
-                    'Primary Table:Left Street ID' <None> VISIBLE NONE;'Primary Table:Right Street ID' <None> VISIBLE NONE;
-                    'Primary Table:Display X' <None> VISIBLE NONE;'Primary Table:Display Y' <None> VISIBLE NONE;
-                    'Primary Table:Min X value for extent' <None> VISIBLE NONE;'Primary Table:Max X value for extent' <None> VISIBLE NONE;
-                    'Primary Table:Min Y value for extent' <None> VISIBLE NONE;'Primary Table:Max Y value for extent' <None> VISIBLE NONE;
-                    'Primary Table:Left Additional Field' <None> VISIBLE NONE;'Primary Table:Right Additional Field' <None> VISIBLE NONE;
-                    'Primary Table:Altname JoinID' RoadCenterline:SEGID VISIBLE NONE;'*Alternate Name Table:JoinID' RoadAlias:SEGID VISIBLE NONE;
-                    'Alternate Name Table:Prefix Direction' RoadAlias:A_PRD VISIBLE NONE;'Alternate Name Table:Prefix Type' RoadAlias:A_STP VISIBLE NONE;
-                    'Alternate Name Table:Street Name' RoadAlias:A_RD VISIBLE NONE;'Alternate Name Table:Suffix Type' RoadAlias:A_STS VISIBLE NONE;
-                    'Alternate Name Table:Suffix Direction' RoadAlias:A_POD VISIBLE NONE"""
-                    CreateAddressLocator_geocoding("US Address - Dual Ranges", streetPath + " 'Primary Table';" + roadAliasPath + " 'Alternate Name Table'", AL4_fieldMap, AL4, "", "DISABLED")
-                except:
-                    userMessage("Could not create county alternate locator from road data")
-
-            if Exists(AL4):
-                userMessage("Created county alternate locator")
-
         #Create composite address locator from addresspoints/road centerline AL3
         if not Exists(AL3):
             userMessage("Creating composite address locator...")
@@ -158,7 +106,7 @@ def createLocators(gdb_object):
                 AL1 + ",City,0,0," + AL2 + ",City,0,0;State \"State\" true true false 20 Text 0 0 ,First,#," + AL1 + ",State,0,0," + AL2 + ",State,0,0;ZIP \"ZIP Code\" true true false 10 Text 0 0 ,First,#," + \
                 AL1 + ",ZIP,0,0," + AL2 + ",ZIP,0,0"
 
-            CreateCompositeAddressLocator_geocoding(AL1 + " AddyPt;" + AL2 + " Roads;" + AL4 + " Alt", compositeFieldMap, "AddyPt #;Roads #;Alt #", AL3)
+            CreateCompositeAddressLocator_geocoding(AL1 + " AddyPt;" + AL2 + " Roads", compositeFieldMap, "AddyPt #;Roads #", AL3)
 
 def prepXLS(tnxls, gdb):
     import xlrd
